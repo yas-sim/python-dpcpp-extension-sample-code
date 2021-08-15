8cf023bff5eea6fc92985948beae82ab26dbe22b
Yasunori Shimura <yasu0710@gmail.com>
2021-08-15 14:24:28 +0900

    add cvtColor to gray input image

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ kernel=np.array([[3,3,3],
 #                 [1,1,1]], dtype=np.float32)
 
 img_in = cv2.imread(image_file)
+img_in = cv2.cvtColor(img_in, cv2.COLOR_BGR2GRAY);
 img_ch = cv2.split(img_in)
 cv2.imshow('input', img_ch[0])
 img_out=image_convolution(img_ch[0], kernel)
